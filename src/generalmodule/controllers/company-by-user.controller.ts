b2cc72e9fa26f85fbd0aa115d78d18c4aaba426b
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2023-06-16 09:51:39 -0700

    Creating controller and service of company by user

--- src/generalmodule/controllers/company-by-user.controller.ts
+++ src/generalmodule/controllers/company-by-user.controller.ts
@@ -1,4 +1,58 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  UseGuards,
+  Post,
+  Body,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { AuthGuard } from '../../auth/guards/auth.guard';
+import { RolesGuard } from '../../auth/guards/roles.guard';
+import { Roles } from '../../auth/decorators/roles.decorator';
+import { CompanyByUserService } from '../services/company-by-user.service';
+import {
+  CreateCompanyByUserDto,
+  UpdateCompanyByUserDto,
+} from '../dto/company-by-user.dto';
 
+@UseGuards(AuthGuard, RolesGuard)
 @Controller('company-by-user')
-export class CompanyByUserController {}
+export class CompanyByUserController {
+  constructor(private readonly companyByUserService: CompanyByUserService) {}
+
+  @Roles('ADMIN')
+  @Post()
+  create(@Body() createCompanyByUserDto: CreateCompanyByUserDto) {
+    return this.companyByUserService.create(createCompanyByUserDto);
+  }
+
+  @Roles('ADMIN', 'BASIC')
+  @Get()
+  findAll() {
+    return this.companyByUserService.findAll();
+  }
+
+  @Roles('ADMIN', 'BASIC')
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: string) {
+    return this.companyByUserService.findOne(+id);
+  }
+
+  @Roles('ADMIN')
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: string,
+    @Body() updateCompanyByUserDto: UpdateCompanyByUserDto,
+  ) {
+    return this.companyByUserService.update(+id, updateCompanyByUserDto);
+  }
+
+  @Roles('ADMIN')
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: string) {
+    return this.companyByUserService.remove(+id);
+  }
+}
--- src/generalmodule/services/company-by-user.service.ts
+++ src/generalmodule/services/company-by-user.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 
 import { CompanyByUser } from '../entities/company-by-user.entity';
 import { ErrorManager } from '../../utils/error.manager';
-import { CreateCompanyByUserDto } from '../dto/company-by-user.dto';
+import {
+  CreateCompanyByUserDto,
+  UpdateCompanyByUserDto,
+} from '../dto/company-by-user.dto';
 
 @Injectable()
 export class CompanyByUserService {
@@ -18,6 +21,98 @@ export class CompanyByUserService {
   async create(createCompanyByUserDto: CreateCompanyByUserDto) {
     try {
       return await this.companyByUserRepository.save(createCompanyByUserDto);
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+
+  //findAll
+  // ...........................................................................
+  async findAll(): Promise<CompanyByUser[]> {
+    try {
+      const companiesByUsers: CompanyByUser[] =
+        await this.companyByUserRepository
+          .createQueryBuilder('companyByUser')
+          .leftJoinAndSelect('companyByUser.company', 'company')
+          .leftJoinAndSelect('companyByUser.user', 'user')
+          .select([
+            'companyByUser.id',
+            'company.id',
+            'company.name',
+            'user.id',
+            'user.username',
+            'companyByUser.createdAt',
+            'companyByUser.updatedAt',
+          ])
+          .getMany();
+      if (!companiesByUsers || companiesByUsers.length === 0) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: 'Company not found',
+        });
+      }
+      return companiesByUsers;
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+
+  //findOne
+  // ...........................................................................
+  async findOne(id: number): Promise<CompanyByUser> {
+    try {
+      const companyByUser: CompanyByUser = await this.companyByUserRepository
+        .createQueryBuilder('companyByUser')
+        .leftJoinAndSelect('companyByUser.company', 'company')
+        .leftJoinAndSelect('companyByUser.user', 'user')
+        .where({ id })
+        .getOne();
+      if (!companyByUser) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: 'Recond not found',
+        });
+      }
+      return companyByUser;
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+
+  //update
+  // ...........................................................................
+  async update(
+    id: number,
+    updateCompanyByUserDto: UpdateCompanyByUserDto,
+  ): Promise<UpdateResult> {
+    try {
+      const companyByUser: UpdateResult =
+        await this.companyByUserRepository.update(id, updateCompanyByUserDto);
+      if (companyByUser.affected === 0) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'Failed to update record',
+        });
+      }
+      return companyByUser;
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+
+  //remove
+  // ...........................................................................
+  async remove(id: number): Promise<DeleteResult> {
+    try {
+      const companyByUser: DeleteResult =
+        await this.companyByUserRepository.delete(id);
+      if (companyByUser.affected === 0) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'Failed to delete record',
+        });
+      }
+      return companyByUser;
     } catch (error) {
       throw ErrorManager.createSignatureError(error.message);
     }
